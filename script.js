8a89573fae57a65861b8613dcdd43bee0eafa372
goldenuzoka <uzokai69@gmail.com>
2023-08-30 15:23:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+
+
+// const fnum = parseInt(prompt('enter first number:'))  //22
+
+// const snum = parseInt(prompt('enter second number:')) //35
+
+
+// const egg = fnum + snum;
+
+// alert('Your first number is ${fnum} and second number is ${snum}\n The total sum is ${egg}')
+
+
+
+// const egg= parseInt(prompt("Enter your age" ))
+// const money = parseInt(prompt("How much do you have?"))
+
+
+
+
+// if(egg >= 18){
+//     alert('You can proceed')
+// }else if(egg> 15 && egg < 18){
+//     alert('Click on Ad to Earn Money!!!')
+// }else{
+//     alert('Parental Guidance Advised')
+// }
+
+
+
+
+
+
+const grade= parseInt(prompt("Enter your score"))
+
+if(grade > 80){
+    alert('A')
+}else if(grade < 80 && grade >= 70){
+    alert("B")
+}else if(grade < 70 && grade >=60){
+    alert("C")
+}else if(grade < 50 && grade >=40 ){
+    alert("D")
+}else if(grade < 40 && grade >=30){
+    alert('you have failed')
+}else{
+    alert('you have no result')
+}
